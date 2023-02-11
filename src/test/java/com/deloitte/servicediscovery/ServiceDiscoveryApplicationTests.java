244fab2d4682b16a5b825b2820ae9e432e9189e0
pswamireddy <pradeep.swamireddy@johnlewis.co.uk>
2023-02-11 15:33:44 +0000

    Initial commit

---
+++ src/test/java/com/deloitte/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
